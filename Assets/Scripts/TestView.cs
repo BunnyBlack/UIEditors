070524da7e07b29715d8a337c97fb1d56e6f8cba
BunnyBlack <xyy6836@126.com>
2022-01-03 22:00:53 +0800

    修改 ShowInInspector 的bug

--- Assets/Scripts/TestView.cs
+++ Assets/Scripts/TestView.cs
@@ -1,9 +1,7 @@
-﻿using Sirenix.OdinInspector;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class TestView : MonoBehaviour
 {
-    [ShowInInspector]
-    private Text desc;
+    public Text _desc;
 }
